ccf918936cd2743e895b155b9baa878a06ea5a79
shizhanhong <Charles.ShiZ@outlook.com>
2021-09-05 11:57:34 +0800

    delete useless files

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,10 @@
 import React, {useState} from "react";
 import ReactDom from "react-dom";
-import CreateRankingList from "./components/createRankingList/index";
 import "./index.css";
 function App(){
   const [visible,setVisible] = useState(false);
   return <div>
-    <CreateRankingList
-      visible={visible}
-      onCancel={() => {
-        setVisible(false);
-      }}
-    />
+	react
   </div>;
 }
 ReactDom.render(<App/>,document.querySelector("#app"));

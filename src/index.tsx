795ceb9a5e26a9b0ed644549f02f028662d416b4
shizhanhong <Charles.ShiZ@outlook.com>
2021-09-16 20:08:27 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import React, {useState} from "react";
+import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
+
 function App(){
-  const [visible,setVisible] = useState(false);
-  return <div>
-	react
-  </div>;
+  return <>react</>;
 }
+
 ReactDom.render(<App/>,document.querySelector("#app"));
